db6674c78d894842f056ed760eb4ce970499e6e4
kareem802 <kareemmustafafoda@gmail.com>
2025-11-12 14:50:39 -0500

    added game board

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,25 @@
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <>
+      <ol id="game-board">
+        {initialGameBoard.map((row, rowIndex) => (
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol, symbolIndex) => (
+                <li key={symbolIndex}>
+                  <button>{playerSymbol}</button>
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+}
